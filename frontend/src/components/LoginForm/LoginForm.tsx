06f37896a7c1b0ea8ab5530dd1a7dfc0327fe99c
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-07-19 19:53:34 -0300

    fix: style sizing

--- frontend/src/components/LoginForm/LoginForm.tsx
+++ frontend/src/components/LoginForm/LoginForm.tsx
@@ -12,8 +12,8 @@ function LoginForm() {
 	};
 
 	return (
-		<div className="grid place-items-center pt-24 pb-16">
-			<div className="flex flex-col gap-2 max-w-sm px-6 py-7 bg-white rounded-xl sm:shadow">
+		<div className="grid place-items-center pt-[80px] pb-12">
+			<div className="flex flex-col gap-2 max-w-sm p-7 bg-white rounded-xl sm:shadow">
 				<h2 className="text-indigo-500 font-bold text-xl">
 				Entrar
 				</h2>
@@ -27,14 +27,14 @@ function LoginForm() {
 					onSubmit={handleSubmit}
 				>
 					<input
-						className="py-2.5 px-3 outline-none rounded-3xl text-sm border-2 border-transparent focus:border-indigo-500 w-full bg-zinc-100"
+						className="py-3 px-3 outline-none rounded-3xl text-sm border-2 border-transparent focus:border-indigo-500 w-full bg-zinc-100"
 						placeholder="Digite seu email..."
 						required
 						type="email"
 					/>
 
 					<button
-						className="p-2.5 bg-indigo-500 text-zinc-50 rounded-3xl hover:bg-indigo-400 text-sm font-medium cursor-pointer flex gap-1 items-center justify-center"
+						className="p-3 bg-indigo-500 text-zinc-50 rounded-3xl hover:bg-indigo-400 text-sm font-medium cursor-pointer flex gap-1 items-center justify-center"
 						type="submit"
 					>
 						Acessar
