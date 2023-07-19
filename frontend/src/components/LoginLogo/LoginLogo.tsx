acbb0bf673aa24290143996c0d4c01df6f1de891
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-07-19 17:26:40 -0300

    feat: create login logo component

---
+++ frontend/src/components/LoginLogo/LoginLogo.tsx
@@ -0,0 +1,12 @@
+import { IonMdAnalytics } from '@/components/_icons/IonMdAnalytics';
+
+function LoginLogo() {
+	return (
+		<h1 className="absolute top-4 left-4 font-bold text-xl flex items-center">
+			<IonMdAnalytics className="text-4xl text-indigo-500 mr-2"/>
+			<span className="text-indigo-500">People</span>Analytics
+		</h1>
+	);
+}
+
+export default LoginLogo;
--- frontend/src/routes/Login/Login.tsx
+++ frontend/src/routes/Login/Login.tsx
@@ -1,9 +1,11 @@
 import LoginForm from '@/components/LoginForm/LoginForm';
+import LoginLogo from '@/components/LoginLogo/LoginLogo';
 import LoginTemplate from '@/components/LoginTemplate/LoginTemplate';
 
 function Login() {
 	return (
-		<main className="bg-zinc-50 page grid place-items-center grid-cols-2">
+		<main className="bg-zinc-50 page grid place-items-center grid-cols-2 relative">
+			<LoginLogo />
 			<LoginForm />
 			<LoginTemplate />
 		</main>
