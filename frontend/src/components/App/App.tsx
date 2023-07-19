f3222fb9e4a6355c1a179e2116f38f32e1c38bfc
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-07-19 16:09:33 -0300

    chore: configure tailwindcss

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,11 +1,9 @@
-import { CssBaseline } from '@mui/material';
 import { RouterProvider } from 'react-router-dom';
 import router from '@/routes';
 
 function App() {
 	return (
 		<>
-			<CssBaseline />
 			<RouterProvider router={router} />
 		</>
 	);
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,4 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import './styles/globals.css';
 import App from './components/App/App';
 import ReactDOM from 'react-dom/client';
 
