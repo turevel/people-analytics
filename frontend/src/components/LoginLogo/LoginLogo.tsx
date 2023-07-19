fc875a61e5a9db870b8c64683b8af7f59bdbec44
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-07-19 19:43:10 -0300

    fix: logo position

--- frontend/src/components/LoginLogo/LoginLogo.tsx
+++ frontend/src/components/LoginLogo/LoginLogo.tsx
@@ -2,7 +2,7 @@ import { IonMdAnalytics } from '@/components/_icons/IonMdAnalytics';
 
 function LoginLogo() {
 	return (
-		<h1 className="absolute top-4 left-4 font-bold text-xl flex items-center">
+		<h1 className="absolute top-4 left-5 font-bold text-xl flex items-center">
 			<IonMdAnalytics className="text-4xl text-indigo-500 mr-2"/>
 			<span className="text-indigo-500">People</span>Analytics
 		</h1>
