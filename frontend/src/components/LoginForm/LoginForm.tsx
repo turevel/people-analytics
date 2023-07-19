78e12151903a9b85e639ceb1aad9d689aa91516c
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-07-19 18:14:49 -0300

    feat: add responsiveness and adjust colors

--- frontend/src/components/LoginForm/LoginForm.tsx
+++ frontend/src/components/LoginForm/LoginForm.tsx
@@ -12,34 +12,36 @@ function LoginForm() {
 	};
 
 	return (
-		<div className="flex flex-col gap-2 max-w-sm px-5 py-7 shadow-md bg-white rounded-xl">
-			<h2 className="text-indigo-500 font-bold text-xl">
+		<div className="grid place-items-center pt-24 pb-16">
+			<div className="flex flex-col gap-2 max-w-sm px-5 py-7 bg-white rounded-xl sm:shadow">
+				<h2 className="text-indigo-500 font-bold text-xl">
 				Entrar
-			</h2>
+				</h2>
 
-			<p className="text-sm mb-2">
-				Acesse suas métricas através de seu e-mail de colaborador!
-			</p>
+				<p className="text-sm mb-2">
+				Acesse suas métricas através do seu e-mail de colaborador!
+				</p>
 
-			<form
-				className="flex flex-col gap-2"
-				onSubmit={handleSubmit}
-			>
-				<input
-					className="py-2.5 px-3 outline-none rounded-3xl text-sm border-2 border-transparent focus:border-indigo-500 w-full bg-zinc-100"
-					placeholder="Digite seu email..."
-					required
-					type="email"
-				/>
-
-				<button
-					className="p-2.5 bg-indigo-500 text-zinc-50 rounded-3xl hover:bg-indigo-400 text-sm font-medium cursor-pointer flex gap-1 items-center justify-center"
-					type="submit"
+				<form
+					className="flex flex-col gap-2"
+					onSubmit={handleSubmit}
 				>
+					<input
+						className="py-2.5 px-3 outline-none rounded-3xl text-sm border-2 border-transparent focus:border-indigo-500 w-full bg-zinc-100"
+						placeholder="Digite seu email..."
+						required
+						type="email"
+					/>
+
+					<button
+						className="p-2.5 bg-indigo-500 text-zinc-50 rounded-3xl hover:bg-indigo-400 text-sm font-medium cursor-pointer flex gap-1 items-center justify-center"
+						type="submit"
+					>
 					Acessar
-					<IcRoundArrowForward className="text-lg"/>
-				</button>
-			</form>
+						<IcRoundArrowForward className="text-lg"/>
+					</button>
+				</form>
+			</div>
 		</div>
 	);
 }
--- frontend/src/routes/Login/Login.tsx
+++ frontend/src/routes/Login/Login.tsx
@@ -4,7 +4,7 @@ import LoginTemplate from '@/components/LoginTemplate/LoginTemplate';
 
 function Login() {
 	return (
-		<main className="bg-zinc-50 page grid place-items-center grid-cols-2 relative">
+		<main className="bg-white sm:bg-zinc-50 page grid place-items-center grid-rows-2-2 relative sm:grid-cols-2">
 			<LoginLogo />
 			<LoginForm />
 			<LoginTemplate />
