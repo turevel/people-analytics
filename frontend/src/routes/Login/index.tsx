671f0e44346650a7e4a36e962f59c0128d2c772c
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-07-19 20:41:32 -0300

    refactor: fix code style

--- frontend/src/routes/Login/index.tsx
+++ frontend/src/routes/Login/index.tsx
@@ -1,5 +1,5 @@
+import About from '@/components/About';
 import LoginForm from '@/components/LoginForm';
-import LoginInterfaceSection from '@/components/LoginInterfaceSection/LoginInterfaceSection';
 import LoginLogo from '@/components/Logo';
 
 function Login() {
@@ -7,7 +7,7 @@ function Login() {
 		<main className="page relative grid place-items-center bg-white lg:grid-cols-2 lg:bg-zinc-50">
 			<LoginLogo />
 			<LoginForm />
-			<LoginInterfaceSection />
+			<About />
 		</main>
 	);
 }
