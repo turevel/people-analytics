e8b2f99df2913cefdf2ea90a1fd16356b7927c9d
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-07-19 20:52:37 -0300

    feat: create not found page

--- frontend/src/routes/NotFound/NotFound.tsx
+++ frontend/src/routes/NotFound/NotFound.tsx
@@ -1,7 +1,17 @@
+import notFound from '@/assets/images/not-found.svg';
+import Logo from '@/components/Logo';
+
 function NotFound() {
 	return (
-		<main>
-      NotFound Page
+		<main className="page relative flex flex-col items-center justify-center bg-zinc-50">
+			<Logo />
+			<img
+				src={notFound}
+				width={300}
+			/>
+			<h1 className="text-sm font-medium">
+        A página que você procura não está aqui!
+			</h1>
 		</main>
 	);
 }
