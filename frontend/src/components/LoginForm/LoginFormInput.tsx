677829872a45d46c8e6e3b26cdbd9c9d86368cb1
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-07-19 21:57:12 -0300

    refactor: placeholder message

--- frontend/src/components/LoginForm/LoginFormInput.tsx
+++ frontend/src/components/LoginForm/LoginFormInput.tsx
@@ -8,7 +8,7 @@ function LoginFormInput({ onChange, value }: Props) {
 		<input
 			className="w-full rounded-3xl border-2 border-transparent bg-zinc-100 px-3 py-3 text-sm outline-none focus:border-indigo-500"
 			onChange={({ target }) => onChange(target.value)}
-			placeholder="Digite seu email..."
+			placeholder="Digite seu e-mail..."
 			required
 			type="email"
 			value={value}
