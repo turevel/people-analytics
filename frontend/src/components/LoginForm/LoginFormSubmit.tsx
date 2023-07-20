e117df60b7225a66947c1177d6b7fa6974acd2d1
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-07-19 21:09:41 -0300

    feat: add email validation

--- frontend/src/components/LoginForm/LoginFormSubmit.tsx
+++ frontend/src/components/LoginForm/LoginFormSubmit.tsx
@@ -1,9 +1,14 @@
 import { IcRoundArrowForward } from '../_icons/IcRoundArrowForward';
 
-function LoginFormSubmit() {
+interface Props {
+  disabled: boolean;
+}
+
+function LoginFormSubmit({ disabled }: Props) {
 	return (
 		<button
-			className="flex cursor-pointer items-center justify-center gap-1 rounded-3xl bg-indigo-500 p-3 text-sm font-medium text-zinc-50 hover:bg-indigo-400"
+			className="flex cursor-pointer items-center justify-center gap-1 rounded-3xl bg-indigo-500 p-3 text-sm font-medium text-zinc-50 hover:bg-indigo-400 disabled:cursor-not-allowed disabled:bg-zinc-300"
+			disabled={disabled}
 			type="submit"
 		>
       Acessar
--- frontend/src/components/LoginForm/index.tsx
+++ frontend/src/components/LoginForm/index.tsx
@@ -1,10 +1,12 @@
 import LoginFormHeader from './LoginFormHeader';
 import LoginFormInput from './LoginFormInput';
 import LoginFormSubmit from './LoginFormSubmit';
-import { FormEvent } from 'react';
+import { FormEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import emailValidator from '@/helpers/emailValidator';
 
 function LoginForm() {
+	const [email, setEmail] = useState('');
 	const navigate = useNavigate();
 
 	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
@@ -21,8 +23,8 @@ function LoginForm() {
 					className="flex flex-col gap-2"
 					onSubmit={handleSubmit}
 				>
-					<LoginFormInput />
-					<LoginFormSubmit />
+					<LoginFormInput value={email} onChange={setEmail} />
+					<LoginFormSubmit disabled={!emailValidator(email)} />
 				</form>
 			</div>
 		</div>
