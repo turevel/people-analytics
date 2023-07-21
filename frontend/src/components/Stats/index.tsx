c1228f17009e2af8e1ed641e706f6f72c6fed3cf
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-07-21 17:39:15 -0300

    fix: padding

--- frontend/src/components/Stats/index.tsx
+++ frontend/src/components/Stats/index.tsx
@@ -3,7 +3,7 @@ import Turnover from './Turnover';
 
 function Stats() {
 	return (
-		<div className="page flex flex-col items-center justify-center gap-5 bg-white pb-10 pt-20 md:bg-zinc-50 md:px-10 lg:px-20 xl:flex-row">
+		<div className="page flex flex-col items-center justify-center gap-5 bg-white pb-10 pt-36 md:bg-zinc-50 md:px-10 lg:px-20 xl:flex-row sm:pt-20">
 			<Headcount />
 			<Turnover />
 		</div>
