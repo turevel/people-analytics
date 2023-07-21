176bbe09363a817864ad1621c6dbb7c21cc65623
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-07-21 16:40:49 -0300

    feat: redirect when data is not available

--- frontend/src/routes/Analytics/Analytics.tsx
+++ frontend/src/routes/Analytics/Analytics.tsx
@@ -1,7 +1,13 @@
+import { Navigate } from 'react-router-dom';
 import Logo from '@/components/Logo';
 import Stats from '@/components/Stats';
+import useMetricsContext from '@/hooks/useMetricsContext';
 
 function Analytics() {
+	const { headcount, turnover } = useMetricsContext();
+
+	if (!headcount && !turnover) return <Navigate to="/login" />;
+
 	return (
 		<main className="page bg-zinc-50">
 			<Logo />
