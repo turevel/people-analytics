53f153799fcc928344718ba64b71c2b6025bdfa4
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-07-21 16:23:32 -0300

    feat: create useRequest hook

---
+++ frontend/src/hooks/useRequest.ts
@@ -0,0 +1,15 @@
+import { AxiosResponse } from 'axios';
+
+function useRequest() {
+	const request = async (callback: () => Promise<AxiosResponse>) => {
+		try {
+			return (await callback()).data;
+		} catch (_) {
+			return null;
+		}
+	};
+
+	return { request };
+}
+
+export default useRequest;
