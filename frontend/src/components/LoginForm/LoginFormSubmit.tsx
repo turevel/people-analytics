1046db97f240b766deeea74f27b1ba2cb2933d39
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-07-21 16:32:34 -0300

    feat: add loading actions with provider

--- frontend/src/components/LoginForm/LoginFormSubmit.tsx
+++ frontend/src/components/LoginForm/LoginFormSubmit.tsx
@@ -1,34 +1,31 @@
 import { IcRoundArrowForward } from '../_icons/IcRoundArrowForward';
 import { LineMdLoadingTwotoneLoop } from '../_icons/LineMdLoadingTwotoneLoop';
+import useMetricsContext from '@/hooks/useMetricsContext';
 
 interface Props {
   disabled: boolean;
 }
 
-const loading = false;
-
 function LoginFormSubmit({ disabled }: Props) {
+	const { loading } = useMetricsContext();
+
 	return (
 		<button
 			className="flex cursor-pointer items-center justify-center gap-1 rounded-3xl bg-indigo-500 p-3 text-sm font-medium text-zinc-50 hover:bg-indigo-400 disabled:cursor-not-allowed disabled:opacity-30"
 			disabled={disabled}
 			type="submit"
 		>
-			{
-				loading
-					? (
-						<>
-							<LineMdLoadingTwotoneLoop className="text-xl" />
-							Aguarde enquanto processamos...
-						</>
-					)
-					: (
-						<>
-							Acessar
-							<IcRoundArrowForward className="text-lg" />
-						</>
-					)
-			}
+			{loading ? (
+				<>
+					<LineMdLoadingTwotoneLoop className="text-xl" />
+          Aguarde enquanto processamos...
+				</>
+			) : (
+				<>
+          Acessar
+					<IcRoundArrowForward className="text-lg" />
+				</>
+			)}
 		</button>
 	);
 }
