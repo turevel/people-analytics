3f48e8f44a866ec87c578002246a051c522879d6
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-07-21 16:54:08 -0300

    feat: add data from provider

--- frontend/src/components/Stats/Headcount.tsx
+++ frontend/src/components/Stats/Headcount.tsx
@@ -1,26 +1,9 @@
 import LineChart from './LineChart';
-
-const data = [
-	{
-		id: 1,
-		data: [
-			{ x: '01-2021', y: 5 },
-			{ x: '02-2021', y: 7 },
-			{ x: '03-2022', y: 12 },
-			{ x: '04-2022', y: 11 },
-			{ x: '05-2023', y: 10 },
-			{ x: '06-2023', y: 13 },
-			{ x: '07-2023', y: 14 },
-			{ x: '08-2023', y: 13 },
-			{ x: '09-2023', y: 15 },
-			{ x: '10-2023', y: 15 },
-			{ x: '11-2023', y: 17 },
-			{ x: '12-2023', y: 17 },
-		],
-	},
-];
+import useMetricsContext from '@/hooks/useMetricsContext';
 
 function Headcount() {
+	const { headcount } = useMetricsContext();
+
 	return (
 		<div className="w-full max-w-xl bg-white px-[22px] sm:p-7 md:rounded-lg md:shadow">
 			<header className="mb-5 space-y-1 text-sm">
@@ -35,7 +18,7 @@ function Headcount() {
 			</header>
 
 			<LineChart
-				data={data}
+				data={headcount}
 				axisBottomLabel="Período"
 				axisLeftLabel="Funcionários ativos"
 			/>
