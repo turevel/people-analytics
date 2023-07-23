dbfae9694235a9e738a0b5956fc4a9c28cdf0bcf
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-07-22 21:56:22 -0300

    feat: create enums

---
+++ backend/src/types/ErrorMessages.ts
@@ -0,0 +1,7 @@
+enum ErrorMessages {
+  EMAIL_NOT_FOUND = 'E-mail não encontrado em nossa base de dados!',
+  BAD_REQUEST = 'Corpo da requisição inválido!',
+  INTERNAL_SERVER_ERROR = 'Erro interno no servidor!'
+}
+
+export default ErrorMessages;
